c039a57ceeb9843ea41f677970bba33f0cb68f55
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-05-13 14:23:10 +0200

    Aggiunto esercizio

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
-<?php
+<?php 
+  class Computer{
+    public $name;
+  }
 
-?>
+  class Portatili extends Computer{
+    public $type;
+    public $price;
+    public $discount;
+
+    public function setType($_type){
+      $this->type = $_type;
+    }
+    public function setPrice($_price){
+      $this->price = $_price;
+    }
+    public function setDiscount($_discount){
+      $this->discount = $this->price * $_discount / 100;
+    }
+
+    public function getType(){
+      return $this->type;
+    }
+    public function getPrice(){
+      return $this->price;
+    }
+    public function getDiscount(){
+      return $this->discount;
+    }
+  }
+
+  $macbook = new Computer();
+
+  $macbookPro = new Portatili();
+  $macbookPro-> setType("Macbook Pro");
+  $macbookPro-> setPrice("2299");
+  $macbookPro-> setDiscount("10");
+  echo "Il tipo di Computer è: " . $macbookPro->getType() . ". ";
+  echo "Il prezzo è: " . $macbookPro->getPrice() . "€. ";
+  echo "Lo sconto è: " . $macbookPro->getDiscount() . "€";
+
+  echo nl2br ("\n");
+
+  $macbookAir = new Portatili();
+  $macbookAir-> setType("Macbook Air");
+  $macbookAir-> setPrice("1499");
+  $macbookAir-> setDiscount("10");
+  echo "Il tipo di Computer è: " . $macbookAir->getType() . ". ";
+  echo "Il prezzo è: " . $macbookAir->getPrice() . "€. ";
+  echo "Lo sconto è: " . $macbookAir->getDiscount() . "€";  
